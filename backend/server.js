eb5a9c220fb0afb4ddaf860e496bb36513834f27
shehneet <127181088+shehneet@users.noreply.github.com>
2024-10-12 10:45:27 +1100

    version 1.0
    square

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Initialise Square client
 const squareClient = new Client({
